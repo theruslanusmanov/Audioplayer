74819cb499122c3ac7fb16b6f728788daab721ca
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-07-29 15:48:43 +0300

    Initial Commit

---
+++ Audioplayer/AudioplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioplayerApp.swift
+//  Audioplayer
+//
+//  Created by Ruslan Usmanov on 2023-07-29.
+//
+
+import SwiftUI
+
+@main
+struct AudioplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioplayerUITests/AudioplayerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AudioplayerUITestsLaunchTests.swift
+//  AudioplayerUITests
+//
+//  Created by Ruslan Usmanov on 2023-07-29.
+//
+
+import XCTest
+
+final class AudioplayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
